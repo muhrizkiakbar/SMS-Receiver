02bf711abb08d866e2e42238c2914307a6ecd25c
muhrizkiakbar <muhrizkiakbar@live.com>
2025-03-26 20:05:40 +0800

    a

--- main.py
+++ main.py
@@ -182,11 +182,11 @@ def parse_sms(sms_text):
     sms_list = []
     try:
         # sms_pattern = r'\+CMGL: \d+,"REC UNREAD","(?P<phone>[\+\d]+)".*?\n(?P<message>.+?)(?=\n\+CMGL|\Z)'
-        # sms_pattern = r'\+CMGL: \d+,"REC READ","(?P<phone>[\+\d]+)",,"(?P<timestamp>[\d/,+]+)"\n(?P<message>(?:.*\n)*?)(?=\n\+CMGL|\Z)'
-        sms_pattern = r'\+CMGL: \d+,"REC UNREAD","(?P<phone>[\+\d]+)",,"(?P<timestamp>[\d/,+]+)"\n(?P<message>.+?)(?=\n\+CMGL|\Z)'
+        sms_pattern = r'\+CMGL: \d+,"REC UNREAD","(?P<phone>\+?\d+)",,"(?P<timestamp>[\d/,:+]+)"\n(?P<message>.*?)(?=\n\+CMGL:|\Z)'
 
-        # matches = re.finditer(sms_pattern, sms_text, re.DOTALL)
-        matches = re.finditer(sms_pattern, sms_text, re.MULTILINE)
+        # Cari semua kecocokan SMS tidak terbaca
+        matches = re.finditer(sms_pattern, sms_text, re.DOTALL)
+
         for match in matches:
             phone_number = match.group("phone")
             message = match.group("message").strip()
