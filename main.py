466ff87fc68a854687ae8a8a801706961e6f8c14
muhrizkiakbar <muhrizkiakbar@live.com>
2025-03-26 14:48:50 +0800

    a

--- main.py
+++ main.py
@@ -184,11 +184,11 @@ def parse_sms(sms_text):
         # sms_pattern = r'\+CMGL: \d+,"REC UNREAD","(?P<phone>[\+\d]+)".*?\n(?P<message>.+?)(?=\n\+CMGL|\Z)'
         sms_pattern = r'\+CMGL: \d+,"REC UNREAD","(?P<phone>[\+\d]+)","",\"(?P<timestamp>\d{2}/\d{2}/\d{2},\d{2}:\d{2}:\d{2}\+\d+)\".*?\n(?P<message>.+?)(?=\n\+CMGL|\Z)'
         matches = re.finditer(sms_pattern, sms_text, re.DOTALL)
-        logging.info("====================")
-        logging.info(f"SMS Text: {sms_text}")
-        logging.info("====================")
-
         for match in matches:
+            logging.info("====================")
+            logging.info(f"SMS Text: {match}")
+            logging.info("====================")
+
             phone_number = match.group("phone")
             message = match.group("message").strip()
             timestamp = match.group("timestamp")
